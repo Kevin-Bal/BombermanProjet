b19a572019c2c330a52c61ca262a9ee74493d496
Etienne <etienne.choveau@hotmail.fr>
2019-11-28 11:44:04 +0100

    Une seule bombe à la fois

--- Fichiers sources Java/Controler/GameState.java
+++ Fichiers sources Java/Controler/GameState.java
@@ -45,43 +45,6 @@ public class GameState {
 		}
 	}
 	
-	public boolean isLegalMove(AgentAction action, Agent agent, int x, int y) {
-		
-		if(map.get_walls()[x][y] || map.getStart_brokable_walls()[x][y] )
-			return false;
-		else return true;
-	}
-	
-	public void moveAgent(AgentAction action, Agent agent) {
-		
-		int x = agent.getX();
-		int y = agent.getY();
-		switch(action) {
-		case MOVE_UP: 
-			x --;
-			break;
-		case MOVE_DOWN:
-			x ++;
-			break;
-		case MOVE_LEFT:
-			y--;
-			break;
-		case MOVE_RIGHT:
-			y++;
-			break;
-		case STOP:
-			break;
-		case PUT_BOMB:
-			break;
-		default :
-			break;
-		}
-		if(isLegalMove( action, agent, x, y)) {
-			agent.setX(x);
-			agent.setY(y);
-			agent.setAgentAction(action);
-		}
-	}
 	
 	public AgentAction GenerateRandomMove() {
 	    int pick = new Random().nextInt(AgentAction.values().length); 
@@ -98,8 +61,10 @@ public class GameState {
 	public void takeTurnEnemies() {
 		for (Agent enemie : enemies) {
 			AgentAction aa=GenerateRandomMove();
-			//System.out.println(aa);
-			moveAgent(aa,enemie);
+			enemie.setAgentAction(aa);
+			if(enemie.isLegalMove(map)) {
+				enemie.executeAction();
+			}
 		}		
 	}
 	
@@ -107,12 +72,49 @@ public class GameState {
 		for (Agent bomberman : bombermans) {
 			Bomberman b = (Bomberman) bomberman;
 			AgentAction aa = GenerateRandomMove();
-			if(aa == AgentAction.PUT_BOMB) {
-				bombs.add(new InfoBomb(bomberman.getX(), bomberman.getY(), b.getRange(), StateBomb.Step1));
+			b.setAgentAction(aa);
+			if(b.isLegalMove(map)) {
+				b.executeAction();
 			}
-			else
-				moveAgent(aa,bomberman);
+			if(aa == AgentAction.PUT_BOMB) {
+				int nbOfBombsPerBomberman = 0;
+				for(InfoBomb bomb : bombs) {
+					if(b.getId()==bomb.getBombermanId())
+						nbOfBombsPerBomberman++;
+				}
+				if(b.getNumberOfBombs()>nbOfBombsPerBomberman) {
+					bombs.add(new InfoBomb(bomberman.getX(), bomberman.getY(), b.getRange(), StateBomb.Step1,b.getId()));
+				}
+			}
 		}		
+	}
+	
+	public void bombTurn() {
+		for(InfoBomb bomb : bombs) {
+			switch(bomb.getStateBomb()) {
+			case Step1 :
+				bomb.setStateBomb(StateBomb.Step2);
+				break;
+			case Step2 :
+				bomb.setStateBomb(StateBomb.Step3);
+				break;
+			case Step3 :
+				bomb.setStateBomb(StateBomb.Boom);
+				break;
+			case Boom :
+				bombsSupprime.add(bomb);
+				isLegalExplosion(bomb.getX(),bomb.getY(),bomb.getRange());
+					
+				break;
+			default :
+				break;
+			}
+		}
+		
+		for(InfoBomb bomb : bombsSupprime) {
+			bombs.remove(bomb);
+		}
+		bombsSupprime.clear();
 	}
 	
 	public void isLegalExplosion(int x, int y, int range) {
@@ -169,36 +171,10 @@ public class GameState {
 				}
 			}
 		}
-	}
-
-	public void bombTurn() {
-		for(InfoBomb bomb : bombs) {
-			switch(bomb.getStateBomb()) {
-			case Step1 :
-				bomb.setStateBomb(StateBomb.Step2);
-				break;
-			case Step2 :
-				bomb.setStateBomb(StateBomb.Step3);
-				break;
-			case Step3 :
-				bomb.setStateBomb(StateBomb.Boom);
-				break;
-			case Boom :
-				bombsSupprime.add(bomb);
-				isLegalExplosion(bomb.getX(),bomb.getY(),bomb.getRange());
-					
-				break;
-			default :
-				break;
-			}
-		}
-		
-		for(InfoBomb bomb : bombsSupprime) {
-			bombs.remove(bomb);
-		}
-		bombsSupprime.clear();
-	}
+	}	
 	
+	//###########################################################################
+	//				GETTERS AND SETTERS
 	public ArrayList<Agent> getBombermans() {
 		return bombermans;
 	}
@@ -245,5 +221,6 @@ public class GameState {
 	public void setBrokable_walls(boolean[][] brokable_walls) {
 		this.brokable_walls = brokable_walls;
 	}
+	//###########################################################################
 	
 }
