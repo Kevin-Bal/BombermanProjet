33a4be57268e2dd343621168f73436a9f1e69bb4
Kevin-Bal <balavoine.kevin@gmail.com>
2019-11-28 11:46:54 +0100

    ajout nouveau jframe

--- Fichiers sources Java/Main/Test.java
+++ Fichiers sources Java/Main/Test.java
@@ -1,12 +1,14 @@
 package Main;
 import Model.BombermanGame;
-import View.ViewCommand;
+import View.PanelCommande;
+import View.ViewGame;
 
 public class Test {
 
 	public static void main(String[] args) throws Exception {
 		BombermanGame sg = new BombermanGame();
-		ViewCommand vc = new ViewCommand(sg);
+		PanelCommande pc = new PanelCommande(sg);
+		ViewGame vc = new ViewGame(sg, pc);
 
 		sg.init();
 	}
