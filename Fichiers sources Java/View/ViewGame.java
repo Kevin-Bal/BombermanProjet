3d619e3d9c54cf5ea6bcaacd854b7a4b6c3fba79
Etienne <etienne.choveau@hotmail.fr>
2019-12-16 13:49:58 +0100

    Gestion de fin du jeu

--- Fichiers sources Java/View/ViewGame.java
+++ Fichiers sources Java/View/ViewGame.java
@@ -72,14 +72,19 @@ public class ViewGame extends JFrame implements Observer{
 		checkEndGame();
 	}
 	
+	
 	/*
 	 * Affiche un popup avec les scores des bombermans quand la partie se termine
 	 */
 	public void checkEndGame(){
 		if(game.isEndgame()) {
-			String text = "";
-			String title ="";
-			if(game.getGameMode()==GameMode.PVE) {				
+			orderByScore(game.getEtatJeu().getBombermans());
+			orderByScore(game.getEtatJeu().getDeadBombermans());
+			
+			//GAME MODE = PVE
+			if(game.getGameMode()==GameMode.PVE) {	
+				ArrayList<String> text = new ArrayList<String>();
+				String title ="";
 				if(game.getEtatJeu().getBombermans().size()<1)
 					title += "Perdu ! \n";
 				else
@@ -87,51 +92,152 @@ public class ViewGame extends JFrame implements Observer{
 				
 				for(Agent b : game.getEtatJeu().getBombermans()) {
 					Bomberman temp = (Bomberman) b;
-					text += "Le Bomberman a fait un score de "+temp.score+"\n";	
+					text.add("Le Bomberman a fait un score de "+temp.score+"\n"); 	
 				}
 				for(Agent b : game.getEtatJeu().getDeadBombermans()) {
 					Bomberman temp = (Bomberman) b;
-					text += "Le Bomberman a fait un score de "+temp.score+"\n";	
+					text.add("Le Bomberman a fait un score de "+temp.score+"\n");	
 				}
+				createPopupForPVE(title,text);
 			}
-			if(game.getGameMode()==GameMode.PVP) {				
-				text += "Gagnants : \n";
+			
+			//GAME MODE = PVP
+			if(game.getGameMode()==GameMode.PVP) {	
+				ArrayList<String> text = new ArrayList<String>();
+				String title ="";
+				ArrayList<String> text2 = new ArrayList<String>();
+				String title2 ="";
+				title = "Gagnant(s) : \n";
 				for(Agent b : game.getEtatJeu().getBombermans()) {
 					Bomberman temp = (Bomberman) b;
-					text += "Le Bomberman "+ temp.getId() +" a fait un score de "+temp.score+"\n";	
+					text.add("Le Bomberman "+ temp.getColor().toString().toLowerCase()  +" a fait un score de "+temp.score+"\n");
 				}
-				text += "Perdants : \n";
+				title2 = "Perdant(s) : \n";
 				for(Agent b : game.getEtatJeu().getDeadBombermans()) {
 					Bomberman temp = (Bomberman) b;
-					text += "Le Bomberman "+ temp.getId() +" a fait un score de "+temp.score+"\n";	
-				}				
+					text2.add("Le Bomberman "+ temp.getColor().toString().toLowerCase() +" a fait un score de "+temp.score+"\n");
+				}
+				
+				createPopupForPVP(title,text,title2,text2);
 			}
-			createPopup(title,text);
+			
+			
 		}
 	}
 	
-	public void createPopup(String title,String text) {
+	
+	/*
+	 * Creates a popup with a title and a text FOR PVE
+	 */
+	public void createPopupForPVE(String title,ArrayList<String> text) {
 		JFrame frame = new JFrame();
 		JPanel panel = new JPanel();
-		JLabel label = new JLabel();
+		panel.setLayout(new GridLayout(text.size()+1,1));
 		
-		JTextArea jtext = new JTextArea();
-		jtext.setText(text);
-
-		label.setText("<html><h1>"+title+"</h1></br></html>");
-
+		//Add title
+		JLabel label = new JLabel();
+		label.setText("<html><h2>"+title+"</h2></html>");
 		panel.add(label);
-		panel.add(jtext);
+		
+		//Add texte
+		for(String t : text) {
+			JLabel labelText = new JLabel();
+			labelText.setText(t);
+			panel.add(labelText);
+		}
+		
+		
+		frame.add(panel);
+	    frame.setSize(400, 200);
+	    frame.setLocationRelativeTo(null);
+	    frame.setVisible(true);
+	}
+	
+	/*
+	 * Creates a popup with a title and a text FOR PVP
+	 */
+	public void createPopupForPVP(String title,ArrayList<String> text,String title2,ArrayList<String> text2) {
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(text.size()+1+text2.size()+1,1));
+		
+		//Add title
+		JLabel label = new JLabel();
+		label.setText("<html><h2>"+title+"</h2></html>");
+		panel.add(label);
+		
+		//Add texte
+		for(String t : text) {
+			JLabel labelText = new JLabel();
+			String color = ColorOfBomberman(t);
+			labelText.setText("<html><p style=\"color:"+color+";\">"+t+"</p></html>");
+			panel.add(labelText);
+		}
+		
+		//Add title
+		JLabel label2 = new JLabel();
+		label2.setText("<html><h2>"+title2+"</h2></html>");
+		panel.add(label2);
+		
+		//Add texte
+		for(String t : text2) {
+			JLabel labelText2 = new JLabel();
+			String color = ColorOfBomberman(t);
+			labelText2.setText("<html><p style=\"color:"+color+";\">"+t+"</p></html>");
+			panel.add(labelText2);
+		}
+		
 		frame.add(panel);
 	    frame.setSize(800, 400);
+	    frame.setLocationRelativeTo(null);
 	    frame.setVisible(true);
 	}
 	
 	
+	/*
+	 * To order a tab of bombermans by score
+	 */
 	public void orderByScore(ArrayList<Agent> bombermans) {
-		for(Agent b : bombermans) {
-			Bomberman temp = (Bomberman) b;
-			//if(temp.score)
+		for(int i=bombermans.size(); i>1; --i) {
+			for(int j=1; j<i; ++j) {
+				Bomberman temp = (Bomberman) bombermans.get(j);
+				Bomberman temp2 = (Bomberman) bombermans.get(j-1);
+				if(temp.score > temp2.score) {
+					bombermans.set(j, temp2);
+					bombermans.set(j-1, temp);
+				}			
+			}
 		}
+	}
+	
+	/*
+	 * Get the color of the bomberman from the text "Le Bomberman bleu a fait un score de "
+	 */
+	public String ColorOfBomberman(String t) {
+		String color="";
+		
+		String[] splitText = t.split(" ");
+		
+		switch(splitText[2]){
+			case "bleu" :
+				color = "blue";
+			break;
+			case "rouge" :
+				color = "red";
+			break;
+			case "vert" :
+				color = "green";
+			break;
+			case "jaune" :
+				color = "yellow";
+			break;
+			case "blanc" :
+				color = "white";
+			break;
+			default :
+				color = "black";
+		}
+		
+		return color;
 	}
 }
